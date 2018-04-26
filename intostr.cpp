d67e82540e29c5a0b2ffb65d9e5005bfb265724d
regostar <renugopal17@hotmail.com>
2018-04-26 14:06:40 +0530

    First commit

---
+++ intostr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+string f( int val )
+{
+	string s;
+   while(val>0)
+   {
+      s=(char)(val%10 +'0')+s;
+      
+      val=val/10;
+     // cout<<s<<endl;
+   }
+   return s;
+}
+
+int main() {
+	cout << f( 63 ) << endl ;
+	// your code goes here
+	return 0;
+}
